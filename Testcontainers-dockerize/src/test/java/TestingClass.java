45406db48ac782fdc3dacd104b5d9e8a235fc795
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-08 15:17:12 +0100

    Added test3() to check db name extracted from container

--- Testcontainers-dockerize/src/test/java/TestingClass.java
+++ Testcontainers-dockerize/src/test/java/TestingClass.java
@@ -3,6 +3,7 @@ import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Testcontainers
@@ -10,15 +11,28 @@ public class TestingClass {
 
     //Once per test class
     @Container
-    private static final MySQLContainer mysql = new MySQLContainer("mysql:latest");
+    private static final MySQLContainer mysql = new MySQLContainer("mysql:latest")
+            .withDatabaseName("Nexthink_db")
+            .withUsername("MNT_backend")
+            .withPassword("FunnyPass");
 
     @Test
     public void test1() {
+        // Check that Junit is working
         assertTrue(true);
     }
 
     @Test
     public void test2() {
+        // Check that the container is running
         assertTrue(mysql.isRunning());
+    }
+
+    @Test
+    public void test3() {
+        // Check db name
+        String databaseName = mysql.getDatabaseName();
+        String expectedDatabaseName = "Nexthink_db";
+        assertEquals(expectedDatabaseName, databaseName);
     }
 }
