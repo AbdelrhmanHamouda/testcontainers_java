a631e28ab02ce9a75f60a5d6f2b73e4b0bfae0ea
AbdelrhmanHamouda <abdelrhman.ahmed@outlook.com>
2020-12-08 13:38:53 +0100

    init project

---
+++ Testcontainers-dockerize/src/test/java/com/nexthink/mnt/AppTest.java
@@ -0,0 +1,18 @@
+package com.nexthink.mnt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+    @Test
+    public void test2() {
+        assertTrue(false);
+    }
+
+}
